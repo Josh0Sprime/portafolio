f97b43827cbe478c73a1cd463c1dc9aa7f2a2b1f
Jorge Cortes <jorge.tomas@redsalud.gov.cl>
2023-10-05 16:33:36 -0300

    merge con desarrollo

---
+++ assets/Desarrollos-42c462e3.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,c as e,e as t}from"./index-ca9a80de.js";const r={},c={class:"desarrollo"},a=t("h1",null,"Desarrollos",-1),n=[a];function _(l,d,i,p,f,h){return s(),e("div",c,n)}const m=o(r,[["render",_]]);export{m as default};
--- assets/Desarrollos-d1b52272.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as s,c as e,e as t}from"./index-f2d3138f.js";const r={},c={class:"desarrollo"},a=t("h1",null,"Desarrollos",-1),n=[a];function _(l,d,i,p,f,h){return s(),e("div",c,n)}const m=o(r,[["render",_]]);export{m as default};
