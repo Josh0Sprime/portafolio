e85ef8b6f1375f3d2e9fbff0c136274b40727581
Jorge Cortes <jorge.tomas@redsalud.gov.cl>
2023-10-05 14:31:43 -0300

    cambio en router

--- src/router/route.js
+++ src/router/route.js
@@ -35,8 +35,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    mode: "history",
+    history: createWebHashHistory(),
     routes,
     linkActiveClass: 'active-link'
 });
